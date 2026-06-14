b8c66d8fc565c9d4fbbc690df6b81cbb90a876c3
Victoria Santander <visant98@gmail.com>
2026-06-14 14:17:30 -0400

    SEO: add sitemap and robots

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default async function sitemap() {
+  return [
+    {
+      url: "https://liverticket.cl",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liverticket.cl/cartelera",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liverticket.cl/crear-evento",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liverticket.cl/contacto",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://liverticket.cl/mis-tickets",
+      lastModified: new Date(),
+    },
+  ];
+}
